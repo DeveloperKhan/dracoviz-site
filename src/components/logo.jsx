5055fadb7252f2d3ea42a98a2f02f2707224774e
DeveloperKhan <adib.khan@gmail.com>
2023-04-27 07:14:14 -0700

    new logo who dis?

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import DracovizLogo from '../../content/assets/dracoviz_logo.svg';
 
 function Logo({ style }) {
   return (
     <div className="logo-wrapper" style={style}>
-      <div className="logo">Dracoviz</div>
+      <img src={DracovizLogo} alt="Dracoviz Logo" className="logo" />
     </div>
   );
 }
