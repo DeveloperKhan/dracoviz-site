54d2a8cef8fb86a84722538c924f8e3f561ceb85
DeveloperKhan <adib.khan@gmail.com>
2023-05-02 02:06:38 -0400

    Fix auth headers part2

--- api/matches.js
+++ api/matches.js
@@ -13,7 +13,7 @@ async function handler(req, res) {
     return;
   }
   const ACTION_KEY = req.headers.authorization.split(" ")[1];
-  if (ACTION_KEY !== process.env.GATSBY_SECRET_KEY) {
+  if (ACTION_KEY !== "667wsLhVIt") {
     res.status(401).json({
       status: 401,
       message: 'Unauthorized'
