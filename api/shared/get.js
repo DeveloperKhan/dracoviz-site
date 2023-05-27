6b26663810398e9bb95137c5c249cbf4885de99f
ShinyDialga <shinydialga45@gmail.com>
2023-05-27 17:11:18 -0500

    Readd authorization

--- api/shared/get.js
+++ api/shared/get.js
@@ -1,45 +1,45 @@
-import getPlayerModel from "../../db/player";
-import getSessionModel from "../../db/session";
-import allowCors from "../../db/allowCors";
+import getPlayerModel from '../../db/player';
+import getSessionModel from '../../db/session';
+import allowCors from '../../db/allowCors';
 
 async function handler(req, res) {
-    const { id } = req.query;
-  //using 'custom' x_authorization header because the regular 'authorization' header is stripped by Vercel in PROD environments.
-  const { x_authorization } = req.headers
+  const { id } = req.query;
+  // using 'custom' x_authorization header because the regular 'authorization' header is stripped by Vercel in PROD environments.
+  const { x_authorization } = req.headers;
   if (x_authorization == null) {
-    // res.status(401).json({
-    //   status: 401,
-    //   message: 'Missing authorization header'
-    // })
-    // return;
+    res.status(401).json({
+      status: 401,
+      message: 'Missing authorization header'
+    })
+    return;
   }
-  // const ACTION_KEY = x_authorization.split(" ")[1];
-  // if (ACTION_KEY !== process.env.GATSBY_SECRET_KEY) {
-  //   // res.status(401).json({
-  //   //   status: 401,
-  //   //   message: 'Unauthorized'
-  //   // })
-  //   // return;
-  // }
-  try {   
-     const Player = await getPlayerModel();
-    var player = await Player.findOne({'session': "n1KWjK6M7LQHTREJsA1j1kcxtsT2"});
+  const ACTION_KEY = x_authorization.split(" ")[1];
+  if (ACTION_KEY !== process.env.GATSBY_SECRET_KEY) {
+    res.status(401).json({
+      status: 401,
+      message: 'Unauthorized'
+    })
+    return;
+  }
+  try {
+    const Player = await getPlayerModel();
+    const player = await Player.findOne({ session: 'n1KWjK6M7LQHTREJsA1j1kcxtsT2' });
     if (player == null || player.length <= 0) {
-      res.status(401).json({ error: `Player not found` });
+      res.status(401).json({ error: 'Player not found' });
       return;
     }
 
     const Session = await getSessionModel();
-    let sessions = [];
-    for (let playerSession of player.sessions) {
-      let session = await Session.findOne({'_id': playerSession});
-      let role = "Player";
+    const sessions = [];
+    for (const playerSession of player.sessions) {
+      const session = await Session.findOne({ _id: playerSession });
+      let role = 'Player';
       if (session.host === player.name) {
-        role = "Host";
+        role = 'Host';
       } else {
-        for (let faction of session.factions) {
+        for (const faction of session.factions) {
           if (faction.captain === player.name) {
-            role = "Captain";
+            role = 'Captain';
             break;
           }
         }
@@ -49,22 +49,22 @@ async function handler(req, res) {
         name: session.name,
         playerValues: {
           status: session.state,
-          role: role,
-          meta: session.metaLogo
-        }
-      })
+          role,
+          meta: session.metaLogo,
+        },
+      });
     }
 
-    let response = {
+    const response = {
       name: player.name,
       description: player.description,
       avatar: player.avatar,
       friendCode: player.friendCode,
       discord: player.discord,
       telegram: player.telegram,
-      sessions: sessions,
-    }
-    res.status(200).json(response)
+      sessions,
+    };
+    res.status(200).json(response);
   } catch (ex) {
     res.status(401).json({ error: `Invalid query of player=${id}` });
   }
