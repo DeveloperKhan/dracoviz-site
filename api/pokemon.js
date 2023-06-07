873ceee0f2f4ae2c89aaf06c401d6cfe1d603365
DeveloperKhan <adib.khan@gmail.com>
2023-06-06 19:25:12 -0700

    Add docs for pokemon api

--- api/pokemon.js
+++ api/pokemon.js
@@ -2,8 +2,8 @@ import pokemon from '../static/pokemon.json';
 import allowCors from '../db/allowCors';
 
 function handler(req, res) {
-  const { meta } = req.query;
-  const { x_authorization } = req.headers;
+  const { session } = req.query;
+  const { x_authorization, x_session_id } = req.headers;
   if (x_authorization == null) {
     res.status(401).json({
       status: 401,
@@ -11,7 +11,21 @@ function handler(req, res) {
     });
     return;
   }
-  // TODO: Get pokemon for specific meta
+  // TODO: Get pokemon for specific meta + session
+  /*
+    1. Do some logic to match player to session
+      a. Check if player is allowed to edit team
+    2. Find player's team for that session
+    3. Find meta the player is registered for
+    4. NICE TO HAVE: only return the Pokemon allowed for that meta
+    5. return:
+        {
+          currentTeam,
+          canEditTeam,
+          meta,
+          pokemon
+        }
+   */
   res.status(200).json(pokemon);
 }
 
