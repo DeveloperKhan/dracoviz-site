84fb3877af0c9ba1902d8d35d43a88ce2d300eb0
ShinyDialga <shinydialga45@gmail.com>
2023-07-09 15:18:45 -0500

    Add update state endpoint

---
+++ api/session/state.js
@@ -0,0 +1,61 @@
+import getPlayerModel from '../../db/player';
+import getSessionModel from '../../db/session';
+import allowCors from '../../db/allowCors';
+
+async function handler(req, res) {
+  const {
+    tournamentId, newState,
+  } = req.body;
+  const { x_authorization, x_session_id } = req.headers;
+  if (x_authorization == null) {
+    res.status(401).json({
+      status: 401,
+      message: 'Missing authorization header',
+    });
+    return;
+  }
+  const ACTION_KEY = x_authorization.split(' ')[1];
+  if (ACTION_KEY !== process.env.GATSBY_SECRET_KEY) {
+    res.status(401).json({
+      status: 401,
+      message: 'Unauthorized',
+    });
+    return;
+  }
+  try {
+    const Player = await getPlayerModel();
+    const player = await Player.findOne({ session: x_session_id });
+    if (player == null || player.length <= 0) {
+      res.status(401).json({ error: 'Cannot log in' });
+      return;
+    }
+
+    const Session = await getSessionModel();
+    const session = await Session.findOne({ key: tournamentId });
+    if (session == null || session.length <= 0) {
+      res.status(401).json({ error: 'Tournament not found' });
+      return;
+    }
+
+    const validStates = ["POKEMON_VISIBLE", "MATCHUPS_VISIBLE", "REGISTER_TEAM", "REGISTER_ROSTER"];
+
+    if (!validStates.includes(newState)) {
+      res.status(401).json({ error: 'Invalid state' });
+      return;
+    }
+
+    if (!session.host.includes(x_session_id)) {
+      res.status(401).json({ error: 'No permission' });
+      return;
+    }
+
+    session.state = newState;
+    await session.save();
+
+    res.status(200).send({});
+  } catch (ex) {
+    res.status(401).json({ error: `Invalid query of session=${tournamentId}` });
+  }
+}
+
+export default allowCors(handler);
