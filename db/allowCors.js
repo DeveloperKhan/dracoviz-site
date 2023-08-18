a62b8b2271614a0470f75ecb37acd3ac249c90a3
DeveloperKhan <adib.khan@gmail.com>
2023-08-18 03:14:43 -0400

    Add Access-Control-Allow-Origin to headers

--- db/allowCors.js
+++ db/allowCors.js
@@ -4,7 +4,7 @@ const allowCors = (fn) => async (req, res) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, x_session_id, x_authorization',
+    'X-CSRF-Token, X-Requested-With, Access-Control-Allow-Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, x_session_id, x_authorization',
   );
   if (req.method === 'OPTIONS') {
     res.status(200).end();
