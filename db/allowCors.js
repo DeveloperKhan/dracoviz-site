0fe5c2461a9a6173498a3710ab91f1b543ceaaaa
DeveloperKhan <adib.khan@gmail.com>
2023-08-18 03:01:02 -0400

    Disable cors

--- db/allowCors.js
+++ db/allowCors.js
@@ -1,16 +1,16 @@
 const allowCors = (fn) => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, x_session_id, x_authorization',
-  );
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  // eslint-disable-next-line consistent-return, no-return-await
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  // res.setHeader(
+  //   'Access-Control-Allow-Headers',
+  //   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, x_session_id, x_authorization',
+  // );
+  // if (req.method === 'OPTIONS') {
+  //   res.status(200).end();
+  //   return;
+  // }
+  // // eslint-disable-next-line consistent-return, no-return-await
   return await fn(req, res);
 };
 
