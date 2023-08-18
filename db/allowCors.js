fae47d9ede96884e81a684a39cf4b727187db82c
DeveloperKhan <adib.khan@gmail.com>
2023-08-18 04:17:03 -0400

    Revert cors changes

--- db/allowCors.js
+++ db/allowCors.js
@@ -1,10 +1,10 @@
 const allowCors = (fn) => async (req, res) => {
-  // res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    '*',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, x_session_id, x_authorization',
   );
   if (req.method === 'OPTIONS') {
     res.status(200).end();
