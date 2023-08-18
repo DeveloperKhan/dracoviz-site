baa49f3d9c5f832272568c59004fe8b2838159e2
DeveloperKhan <adib.khan@gmail.com>
2023-08-18 04:36:29 -0400

    remove id query param

--- api/shared/get.js
+++ api/shared/get.js
@@ -5,7 +5,7 @@ import avatars from '../../static/avatars.json';
 import rules from '../../static/rules.json';
 
 async function handler(req, res) {
-  const { id } = req.query;
+  // const { id } = req.query;
   const { x_authorization, x_session_id } = req.headers;
   if (x_authorization == null) {
     res.status(401).json({
@@ -24,50 +24,45 @@ async function handler(req, res) {
   }
   try {
     const Player = await getPlayerModel();
-    const isIdValid = id == null || id === '';
-    const player = isIdValid
-      ? await Player.findOne({ session: x_session_id })
-      : await Player.findOne({ name: id });
+    const player = await Player.findOne({ session: x_session_id });
     if (player == null || player.length <= 0) {
       res.status(401).json({ error: 'api_player_not_found' });
       return;
     }
 
     const sessions = [];
-    if (id == null) {
-      const Session = await getSessionModel();
-      await Promise.all(player.sessions.map(async (playerSession) => {
-        try {
-          const session = await Session.findOne({ key: playerSession });
-          let role = 'Player';
-          if (session.host.includes(player.session)) {
-            role = 'Host';
-          } else {
-            session.factions.forEach((faction) => {
-              if (faction.captain === player.session) {
-                role = 'Captain';
-              }
-            });
-          }
-          const {
-            key, name, state, metas, currentRoundNumber,
-          } = session;
-          const rule = rules[metas[0]];
-          sessions.push({
-            _id: key,
-            name,
-            currentRoundNumber,
-            playerValues: {
-              status: state,
-              role,
-              meta: rule.metaLogo,
-            },
+    const Session = await getSessionModel();
+    await Promise.all(player.sessions.map(async (playerSession) => {
+      try {
+        const session = await Session.findOne({ key: playerSession });
+        let role = 'Player';
+        if (session.host.includes(player.session)) {
+          role = 'Host';
+        } else {
+          session.factions.forEach((faction) => {
+            if (faction.captain === player.session) {
+              role = 'Captain';
+            }
           });
-        } catch (error) {
-        // Error finding session
         }
-      }));
-    }
+        const {
+          key, name, state, metas, currentRoundNumber,
+        } = session;
+        const rule = rules[metas[0]];
+        sessions.push({
+          _id: key,
+          name,
+          currentRoundNumber,
+          playerValues: {
+            status: state,
+            role,
+            meta: rule.metaLogo,
+          },
+        });
+      } catch (error) {
+        // Error finding session
+      }
+    }));
 
     const response = {
       name: player.name,
@@ -80,7 +75,7 @@ async function handler(req, res) {
     };
     res.status(200).json(response);
   } catch (ex) {
-    res.status(401).json({ error: `Invalid query of player=${id}` });
+    res.status(401).json({ error: 'Invalid query of player' });
   }
 }
 
