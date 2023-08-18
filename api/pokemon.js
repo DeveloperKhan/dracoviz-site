dd54a3994f98fb33a062d2dc11769e3296a2bf57
DeveloperKhan <adib.khan@gmail.com>
2023-08-18 01:33:37 -0400

    Add team registration default

--- api/pokemon.js
+++ api/pokemon.js
@@ -1,8 +1,11 @@
-import pokemon from '../static/pokemon.json';
+import pokemonJSON from '../static/pokemon.json';
 import allowCors from '../db/allowCors';
+import getPlayerModel from '../db/player';
+import getSessionModel from '../db/session';
+import sessionStates from '../db/sessionStates';
 
-function handler(req, res) {
-  const { session } = req.query;
+async function handler(req, res) {
+  const { tournamentId } = req.query;
   const { x_authorization, x_session_id } = req.headers;
   if (x_authorization == null) {
     res.status(401).json({
@@ -10,6 +13,61 @@ function handler(req, res) {
       message: 'api_authorization_missing',
     });
     return;
+  }
+  try {
+    const Player = await getPlayerModel();
+    const player = await Player.findOne({ session: x_session_id });
+    if (player == null || player.length <= 0) {
+      res.status(401).json({ error: 'api_cannot_log_in' });
+      return;
+    }
+
+    const Session = await getSessionModel();
+    const session = await Session.findOne({ key: tournamentId });
+    if (session == null) {
+      res.status(401).json({ error: 'api_session_not_found' });
+      return;
+    }
+
+    const { players, state } = session;
+
+    const playerIndex = players.findIndex((p) => p.playerId === x_session_id);
+
+    if (playerIndex <= -1) {
+      res.status(401).json({ error: 'api_player_not_found' });
+      return;
+    }
+
+    const canEdit = state !== sessionStates.pokemonVisible;
+    const movesRequired = true;
+    const cpRequired = true;
+
+    const pokemon = [];
+    const cp = [];
+    const chargedMoves = [];
+    const fastMoves = [];
+
+    const thePlayer = players[playerIndex];
+
+    thePlayer.pokemon.forEach((p) => {
+      pokemon.push(p.speciesName ?? '');
+      cp.push(p.cp ?? '');
+      chargedMoves.push(p.chargedMoves ?? []);
+      fastMoves.push(p.fastMove);
+    });
+
+    res.status(200).json({
+      canEdit,
+      movesRequired,
+      cpRequired,
+      pokemon,
+      cp,
+      chargedMoves,
+      fastMoves,
+      pokemonData: pokemonJSON,
+    });
+  } catch (ex) {
+    res.status(401).json({ error: 'api_unauthorized' });
   }
   // TODO: Get pokemon for specific meta + session
   /*
@@ -26,7 +84,6 @@ function handler(req, res) {
           pokemon
         }
    */
-  res.status(200).json(pokemon);
 }
 
 export default allowCors(handler);
