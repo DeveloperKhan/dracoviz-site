c92d3b2ca50a157443d552bde081ed73d01c34ca
DeveloperKhan <adib.khan@gmail.com>
2023-10-01 10:32:25 -0700

    Fix navbar

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -4,7 +4,7 @@ import DracovizLogo from '../../content/assets/dracoviz_logo.svg';
 function Logo({ style }) {
   return (
     <div className="logo-wrapper" style={style}>
-      <img src={DracovizLogo} alt="Dracoviz Logo" className="logo" width={170} style={{ marginRight: '20vw' }} />
+      <img src={DracovizLogo} alt="Dracoviz Logo" className="logo" width={170} style={{ marginRight: '8vw' }} />
     </div>
   );
 }
