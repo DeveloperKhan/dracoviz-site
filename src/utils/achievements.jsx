06847b4e291a5988cd2e5ecd6dd47e4a3486f3d1
ShinyDialga <shinydialga45@gmail.com>
2023-10-01 13:23:20 -0500

    new achievements

--- src/utils/achievements.jsx
+++ src/utils/achievements.jsx
@@ -14,6 +14,10 @@ import dracoviz_badges_int4 from '../../static/achievements/dracoviz_badges_int4
 import dracoviz_badges_int8 from '../../static/achievements/dracoviz_badges_int8.png';
 import dracoviz_badges_int16 from '../../static/achievements/dracoviz_badges_int16.png';
 import dracoviz_badges_intchamp from '../../static/achievements/dracoviz_badges_intchamp.png';
+import dracoviz_badges_quest_8x3 from '../../public/achievements/dracoviz_badges_quest_8x3.png';
+import dracoviz_badges_quest_8x4 from '../../public/achievements/dracoviz_badges_quest_8x4.png';
+import dracoviz_badges_quest_8x5 from '../../public/achievements/dracoviz_badges_quest_8x5.png';
+import dracoviz_badges_quest_80 from '../../static/achievements/dracoviz_badges_quest_80.png';
 import dracoviz_badges_quest_90 from '../../static/achievements/dracoviz_badges_quest_90.png';
 import dracoviz_badges_quest_100 from '../../public/achievements/dracoviz_badges_quest_100.png';
 import dracoviz_badges_reg1 from '../../static/achievements/dracoviz_badges_reg1.png';
@@ -200,11 +204,20 @@ function is2024Qualified(profile) {
   return !!profile.tournaments?.some((tournament) => tournament.tournament.includes('2024') && ((tournament.tournament.includes('worlds')) || tournament.qualified));
 }
 
+function is80WinPercent(profile) {
+  return !!profile.tournaments?.some((tournament) => {
+    const winPercentage = (parseInt(tournament.game_wins, 10) + parseInt(tournament.game_losses, 10) > 0
+     ? (100 * parseInt(tournament.game_wins, 10) / (parseInt(tournament.game_wins, 10) + parseInt(tournament.game_losses, 10)))
+      : 0);
+    return tournament.final_rank === 1 && winPercentage >= 80;
+  });
+}
+
 function is90WinPercent(profile) {
   return !!profile.tournaments?.some((tournament) => {
-    const winPercentage = (parseInt(tournament.game_wins, 10) + parseInt(tournament.game_losses, 10) > 0 ?
-     (100 * parseInt(tournament.game_wins, 10) / (parseInt(tournament.game_wins, 10) + parseInt(tournament.game_losses, 10))) :
-      0);
+    const winPercentage = (parseInt(tournament.game_wins, 10) + parseInt(tournament.game_losses, 10) > 0
+     ? (100 * parseInt(tournament.game_wins, 10) / (parseInt(tournament.game_wins, 10) + parseInt(tournament.game_losses, 10)))
+      : 0);
     return tournament.final_rank === 1 && winPercentage >= 90;
   });
 }
@@ -213,108 +226,56 @@ function is100WinPercent(profile) {
   return !!profile.tournaments?.some((tournament) => (tournament.final_rank == 1 && tournament.game_losses <= 0));
 }
 
-// tournament
+function isTop8x3(profile) {
+  const top8Counts = {}; // Store the count of top 8 appearances by year
 
-// function isTop1Lb(profile) {
-//   return !!profile.gbl?.some((gbl) => gbl.peakRank == 1);
-// }
+  // Check each tournament
+  profile.tournaments?.forEach((tournament) => {
+    const tournamentYear = tournament.tournament.split('-')[0]; // Extract the year
 
-// function isLb(profile) {
-//   return profile.gbl !== undefined;
-// }
+    // Increment the count for the corresponding year
+    if (tournament.final_rank <= 8) {
+      top8Counts[tournamentYear] = (top8Counts[tournamentYear] || 0) + 1;
+    }
+  });
 
-// function isQualified(profile) {
-//   return !!profile.tournaments?.some((tournament) => tournament.qualified);
-// }
+  // Check if any year has three or more top 8 appearances
+  return Object.values(top8Counts).some((count) => count >= 3);
+}
 
-// function isTop8(profile) {
-//   return !!profile.tournaments?.some((tournament) => tournament.final_rank <= 8);
-// }
+function isTop8x4(profile) {
+  const top8Counts = {}; // Store the count of top 8 appearances by year
 
-// function isTop8x2(profile) {
-//   // Filter the tournaments where the player got top 8
-//   const top8Tournaments = (profile.tournaments || []).filter((tournament) => tournament.final_rank <= 8);
+  // Check each tournament
+  profile.tournaments?.forEach((tournament) => {
+    const tournamentYear = tournament.tournament.split('-')[0]; // Extract the year
 
-//   // Check if there are at least 2 tournaments with top 8 finishes
-//   return top8Tournaments.length >= 2;
-// }
+    // Increment the count for the corresponding year
+    if (tournament.final_rank <= 8) {
+      top8Counts[tournamentYear] = (top8Counts[tournamentYear] || 0) + 1;
+    }
+  });
 
-// function isTop8x3(profile) {
-//   // Filter the tournaments where the player got top 8
-//   const top8Tournaments = (profile.tournaments || []).filter((tournament) => tournament.final_rank <= 8);
+  // Check if any year has three or more top 8 appearances
+  return Object.values(top8Counts).some((count) => count >= 4);
+}
 
-//   // Check if there are at least 3 tournaments with top 8 finishes
-//   return top8Tournaments.length >= 3;
-// }
+function isTop8x5(profile) {
+  const top8Counts = {}; // Store the count of top 8 appearances by year
 
-// function isTop8x4(profile) {
-//   // Filter the tournaments where the player got top 8
-//   const top8Tournaments = (profile.tournaments || []).filter((tournament) => tournament.final_rank <= 8);
+  // Check each tournament
+  profile.tournaments?.forEach((tournament) => {
+    const tournamentYear = tournament.tournament.split('-')[0]; // Extract the year
 
-//   // Check if there are at least 4 tournaments with top 8 finishes
-//   return top8Tournaments.length >= 4;
-// }
+    // Increment the count for the corresponding year
+    if (tournament.final_rank <= 8) {
+      top8Counts[tournamentYear] = (top8Counts[tournamentYear] || 0) + 1;
+    }
+  });
 
-// function is4th(profile) {
-//   return !!profile.tournaments?.some((tournament) => tournament.final_rank == 4);
-// }
-
-// function is3rd(profile) {
-//   return !!profile.tournaments?.some((tournament) => tournament.final_rank == 3);
-// }
-
-// function is2nd(profile) {
-//   return !!profile.tournaments?.some((tournament) => tournament.final_rank == 2);
-// }
-
-// function is1st(profile) {
-//   return !!profile.tournaments?.some((tournament) => tournament.final_rank == 1);
-// }
-
-// function isWorlds4th(profile) {
-//   return !!profile.tournaments?.some(
-//     (tournament) => tournament.final_rank == 4 && tournament.tournament.includes('worlds'),
-//   );
-// }
-
-// function isWorlds3rd(profile) {
-//   return !!profile.tournaments?.some(
-//     (tournament) => tournament.final_rank == 3 && tournament.tournament.includes('worlds'),
-//   );
-// }
-
-// function isWorlds2nd(profile) {
-//   return !!profile.tournaments?.some(
-//     (tournament) => tournament.final_rank == 2 && tournament.tournament.includes('worlds'),
-//   );
-// }
-
-// function isWorlds1st(profile) {
-//   return !!profile.tournaments?.some(
-//     (tournament) => tournament.final_rank == 1 && tournament.tournament.includes('worlds'),
-//   );
-// }
-
-// function isUndefeated(profile) {
-//   return !!profile.tournaments?.some((tournament) => (tournament.final_rank == 1 && tournament.game_losses <= 0));
-// }
-
-// function isTop8x3Season(profile) {
-//   const top8Counts = {}; // Store the count of top 8 appearances by year
-
-//   // Check each tournament
-//   profile.tournaments?.forEach((tournament) => {
-//     const tournamentYear = tournament.tournament.split('-')[0]; // Extract the year
-
-//     // Increment the count for the corresponding year
-//     if (tournament.final_rank <= 8) {
-//       top8Counts[tournamentYear] = (top8Counts[tournamentYear] || 0) + 1;
-//     }
-//   });
-
-//   // Check if any year has three or more top 8 appearances
-//   return Object.values(top8Counts).some((count) => count >= 3);
-// }
+  // Check if any year has three or more top 8 appearances
+  return Object.values(top8Counts).some((count) => count >= 3);
+}
 
 const achievements = [
   {
@@ -540,6 +501,15 @@ const achievements = [
     image: dracoviz_badges_worlds2024,
   },
   {
+    tier: 1,
+    id: 'is80WinPercent',
+    name: '80% Win Rate',
+    description: 'Win a Play! Pokémon event with a 80%+ game win rate',
+    check: is80WinPercent,
+    image: dracoviz_badges_quest_80,
+  },
+  {
+    tier: 2,
     id: 'is90WinPercent',
     name: '90% Win Rate',
     description: 'Win a Play! Pokémon event with a 90%+ game win rate',
@@ -547,164 +517,38 @@ const achievements = [
     image: dracoviz_badges_quest_90,
   },
   {
+    tier: 3,
     id: 'is100WinPercent',
     name: '100% Win Rate',
     description: 'Win a Play! Pokémon event with a 100% game win rate',
     check: is100WinPercent,
     image: dracoviz_badges_quest_100,
   },
+  {
+    tier: 1,
+    id: 'top8x',
+    name: 'Top 8 x 3',
+    description: 'Three Top 8 finishes at Play! events in a single season',
+    check: isTop8x3,
+    image: dracoviz_badges_quest_8x3,
+  },
+  {
+    tier: 2,
+    id: 'top8x',
+    name: 'Top 8 x 4',
+    description: 'Four Top 8 finishes at Play! events in a single season',
+    check: isTop8x4,
+    image: dracoviz_badges_quest_8x4,
+  },
+  {
+    tier: 3,
+    id: 'top8x',
+    name: 'Top 8 x 5',
+    description: 'Five Top 8 finishes at Play! events in a single season',
+    check: isTop8x5,
+    image: dracoviz_badges_quest_8x5,
+  },
 ];
-
-// const achievements = [
-//   {
-//     tier: 1,
-//     id: 'is3500',
-//     name: '3500 Rating',
-//     description: 'Reach 3500 rating',
-//     check: is3500,
-//     image: dracoviz_badges_gblLB500,
-
-//   },
-//   {
-//     id: 'isTop50Lb',
-//     name: 'Page 1 GBL',
-//     description: 'Reach top 50 in the GBL leaderboard',
-//     check: isTop50Lb,
-//     image: top50,
-//   },
-//   {
-//     id: 'isTop1Lb',
-//     name: '#1 GBL',
-//     description: 'Reach #1 in the GBL leaderboard',
-//     check: isTop1Lb,
-//     image: number1,
-//   },
-//   {
-//     id: 'isLb',
-//     name: 'GBL Leaderboard',
-//     description: 'Reach the GBL leaderboard',
-//     check: isLb,
-//     image: lb,
-//   },
-//   {
-//     id: 'qualified',
-//     name: 'Worlds Qualified',
-//     description: 'Qualify for the World Championships',
-//     check: isQualified,
-//     image: qualified,
-//   },
-//   {
-//     tier: 1,
-//     id: 'top8',
-//     name: 'Top 8',
-//     description: 'Top 8 at a Play! Pokemon event',
-//     check: isTop8,
-//     image: dracoviz_badges_gblLB500.png,
-//   },
-//   {
-//     tier: 2,
-//     id: 'top8',
-//     name: 'Top 8 x2',
-//     description: 'Top 8 x2 at a Play! Pokemon event',
-//     check: isTop8x2,
-//     image: dracoviz_badges_gblLB500.png,
-//   },
-//   {
-//     tier: 3,
-//     id: 'top8',
-//     name: 'Top 8 x3',
-//     description: 'Top 8 x3 at a Play! Pokemon event',
-//     check: isTop8x3,
-//     image: dracoviz_badges_gblLB500.png,
-//   },
-//   {
-//     tier: 4,
-//     id: 'top8',
-//     name: 'Top 8 x4',
-//     description: 'Top 8 x4 at a Play! Pokemon event',
-//     check: isTop8x4,
-//     image: dracoviz_badges_gblLB500.png,
-//   },
-//   {
-//     tier: 1,
-//     id: 'bigPlacement',
-//     name: '4th Place',
-//     description: '4th Place at a Play! Pokemon event',
-//     check: is4th,
-//     image: dracoviz_badges_gblLB500.png,
-//   },
-//   {
-//     tier: 2,
-//     id: 'bigPlacement',
-//     name: '3rd Place',
-//     description: '3rd Place at a Play! Pokemon event',
-//     check: is3rd,
-//     image: dracoviz_badges_gblLB500.png,
-//   },
-//   {
-//     tier: 3,
-//     id: 'bigPlacement',
-//     name: '2nd Place',
-//     description: '2nd Place at a Play! Pokemon event',
-//     check: is2nd,
-//     image: dracoviz_badges_gblLB500.png,
-//   },
-//   {
-//     tier: 4,
-//     id: 'bigPlacement',
-//     name: '1st Place',
-//     description: '1st Place at a Play! Pokemon event',
-//     check: is1st,
-//     image: dracoviz_badges_gblLB500.png,
-//   },
-//   {
-//     tier: 1,
-//     id: 'worlds',
-//     name: '4th Worlds',
-//     description: '4th Place at the World Championship',
-//     check: isWorlds4th,
-//     image: dracoviz_badges_gblLB500.png,
-//   },
-//   {
-//     tier: 2,
-//     id: 'worlds',
-//     name: '3rd Worlds',
-//     description: '3rd Place at the World Championship',
-//     check: isWorlds3rd,
-//     image: dracoviz_badges_gblLB500.png,
-//   },
-//   {
-//     tier: 3,
-//     id: 'worlds',
-//     name: '2nd Worlds',
-//     description: '2nd Place at the World Championship',
-//     check: isWorlds2nd,
-//     image: dracoviz_badges_gblLB500.png,
-//   },
-//   {
-//     tier: 4,
-//     id: 'worlds',
-//     name: '1st Worlds',
-//     description: '1st Place at the World Championship',
-//     check: isWorlds1st,
-//     image: dracoviz_badges_gblLB500.png,
-//   },
-//   {
-//     tier: 4,
-//     id: 'top8x3Season',
-//     name: 'Season Top 8 x3',
-//     description: 'Reach top 8 at a Play! Pokemon event 3 times in a single season',
-//     check: isTop8x3Season,
-//     image: dracoviz_badges_gblLB500.png,
-//   },
-//   {
-//     id: 'undefeated',
-//     name: 'Undefeated',
-//     description: 'Never lose a single game at a Play! Pokemon event',
-//     check: isUndefeated,
-//     image: dracoviz_badges_gblLB500.png,
-//   },
-// ];
 
 export function getAchievements(profile) {
   const highestTierAchievements = {};
