7d596db463a3848946b655a9ffd26ff6acda6195
DeveloperKhan <adib.khan@gmail.com>
2023-10-02 18:12:15 -0700

    Add conclude tournament bracket

--- api/session/conclude.js
+++ api/session/conclude.js
@@ -1,6 +1,34 @@
 import getSessionModel from '../../db/session';
 import getPlayerModel from '../../db/player';
 import allowCors from '../../db/allowCors';
+import bracketTypes from '../../db/bracketTypes';
+
+function getResults(matches, playerIdToSearch) {
+  let wins = 0;
+  let losses = 0;
+  let gameWins = 0;
+  let gameLosses = 0;
+  let opponent = null;
+
+  matches.forEach((match) => {
+    match.participants.forEach((participantsArray, i) => {
+      participantsArray.forEach((participant, j) => {
+        if (participant.playerId === playerIdToSearch) {
+          const opponentIndex = participantsArray[1 - j];
+          opponent = participantsArray[opponentIndex]?.playerId ?? null;
+          gameWins = match.score[i][j];
+          gameLosses = match.score[i][1 - j];
+          wins = gameWins > gameLosses ? 1 : 0;
+          losses = gameLosses > gameWins ? 1 : 0;
+        }
+      });
+    });
+  });
+
+  return {
+    wins, losses, gameWins, gameLosses, opponent,
+  };
+}
 
 async function handler(req, res) {
   const { tournamentId } = req.body;
@@ -42,6 +70,34 @@ async function handler(req, res) {
     if (session.concluded) {
       res.status(401).json({ error: 'api_session_concluded' });
       return;
+    }
+
+    if (session.bracketType === bracketTypes.swiss
+      && session.totalRounds === session.currentRoundNumber) {
+      const currentRoundIndex = session.bracket.findIndex(
+        (r) => r.round === session.currentRoundNumber,
+      );
+      const currentRound = session.bracket[currentRoundIndex];
+      if (currentRound == null) {
+        session.concluded = true;
+        await session.save();
+        res.status(200).send({});
+        return;
+      }
+      session.players.forEach((p, i) => {
+        const {
+          wins, losses, gameWins, gameLosses, opponent,
+        } = getResults(currentRound.matches, p.playerId);
+        session.players[i].wins = (session.players[i].wins ?? 0) + wins;
+        session.players[i].losses = (session.players[i].losses ?? 0) + losses;
+        session.players[i].gameWins = (session.players[i].gameWins ?? 0) + gameWins;
+        session.players[i].gameLosses = (session.players[i].gameLosses ?? 0) + gameLosses;
+        if (opponent == null) {
+          return;
+        }
+        session.players[i].opponents.push(opponent);
+        session.players[i].receivedBye = true;
+      });
     }
 
     session.concluded = true;
