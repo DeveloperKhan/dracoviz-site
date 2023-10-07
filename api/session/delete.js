899fc652b6f10df0180823a9b31548ffab8cc5dd
DeveloperKhan <adib.khan@gmail.com>
2023-10-07 16:31:18 -0700

    Add delete endpoint

---
+++ api/session/delete.js
@@ -0,0 +1,56 @@
+import getSessionModel from '../../db/session';
+import getPlayerModel from '../../db/player';
+import allowCors from '../../db/allowCors';
+
+async function handler(req, res) {
+  const {
+    tournamentId,
+  } = req.body;
+  const { x_authorization, x_session_id } = req.headers;
+  if (x_authorization == null) {
+    res.status(401).json({
+      status: 401,
+      message: 'api_authorization_missing',
+    });
+    return;
+  }
+  const ACTION_KEY = x_authorization.split(' ')[1];
+  if (ACTION_KEY !== process.env.GATSBY_SECRET_KEY) {
+    res.status(401).json({
+      status: 401,
+      message: 'api_unauthorized',
+    });
+    return;
+  }
+  try {
+    const Player = await getPlayerModel();
+    const player = await Player.findOne({ session: x_session_id });
+    if (player == null || player.length <= 0) {
+      res.status(401).json({ error: 'api_player_not_found' });
+      return;
+    }
+    const Session = await getSessionModel();
+    const session = await Session.findOne({ key: tournamentId });
+    if (session == null || session.length <= 0) {
+      res.status(401).json({ error: 'api_session_not_found' });
+      return;
+    }
+
+    if (session.concluded) {
+      res.status(401).json({ error: 'api_session_concluded' });
+      return;
+    }
+
+    if (!session.host.includes(x_session_id)) {
+      res.status(401).json({ error: 'api_unauthorized' });
+      return;
+    }
+    await Session.findOneAndDelete({ key: tournamentId });
+    res.status(200).send({});
+  } catch (ex) {
+    console.error(ex);
+    res.status(401).json({ error: 'api_unauthorized' });
+  }
+}
+
+export default allowCors(handler);
