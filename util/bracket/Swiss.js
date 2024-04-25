a4e6f2a8344a952e2688d95933ba62e975ebe595
DeveloperKhan <adib.khan@gmail.com>
2024-04-25 00:25:53 -0700

    change swiss 2

--- util/bracket/Swiss.js
+++ util/bracket/Swiss.js
@@ -74,10 +74,10 @@ export default function Swiss(players, round, rated = false, colors = false) {
           (s) => s === opp.score,
         ),
       );
-      wt += scoreGroupDiff < 1
+      wt += scoreGroupDiff < 2
         ? 3 / Math.log10(scoreGroupDiff + 2)
         : 1 / Math.log10(scoreGroupDiff + 2);
-      if (highest === Math.max(curr.score, opp.score)) {
+      if (round > 2 && highest === Math.max(curr.score, opp.score)) {
         if (scoreGroupDiff === 1) {
           wt += 33;
         }
